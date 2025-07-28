ccb82e369f84a2e6e89e12a4f21afb13444ca35d
KotoFeelGood72 <kotenkolove@yandex.ru>
2025-07-28 18:00:24 +0300

    Добавил страницу контакты

---
+++ app/components/cards/ContactsCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="dots"></div>
+    <div>
+      <h3>{{ card.title }}</h3>
+    </div>
+    <ul v-if="!card.requisites">
+      <li v-for="item in card.info">
+        <div v-html="item.action"></div>
+      </li>
+    </ul>
+    <div v-if="card.social">
+      <IconTelegram />
+      <IconWhatsapp />
+    </div>
+    <div v-if="card.requisites">
+      <div v-for="item in card.organization">
+        <p>{{ item.label }}</p>
+        <p>{{ item.inn }}</p>
+        <p>{{ item.ogrn }}</p>
+      </div>
+      <Btn name="Скачать реквизиты" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import IconTelegram from "@/components/icons/icon-telegram.vue";
+import IconWhatsapp from "@/components/icons/icon-whatsapp.vue";
+import Btn from "../buttons/Btn.vue";
+const props = defineProps<{
+  card: any;
+}>();
+</script>
--- app/pages/contacts.vue
+++ app/pages/contacts.vue
@@ -1,5 +1,15 @@
-<template>Contacts</template>
+<template>
+  <div class="container">
+    <ContactsCard :card="contact" v-for="contact in contacts" :key="contact.title" />
+    <div>
+      <NuxtImg src="/images/map.png" alt="map" />
+    </div>
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ContactsCard from "@/components/cards/ContactsCard.vue";
+import { contacts } from "@/data/Contacts";
+</script>
 
 <style scoped></style>
