52558648dab0019957e71730efecc2f65d9233fe
KotoFeelGood72 <kotenkolove@yandex.ru>
2025-08-04 18:46:44 +0300

    Начал делать страницу продукта

---
+++ app/components/shared/ProductSlider.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="product-view">
+    <div>
+      <PageHead
+        :breadcrumbs="[
+          { label: 'Главная', to: '/' },
+          {
+            label: 'Каталог',
+            children: [
+              { label: 'Полиграфия', to: '/shop' },
+              { label: 'Баннеры', to: '/shop/banners' },
+              { label: 'Буклеты', to: '/shop/booklets' },
+              { label: 'Визитки', to: '/shop/visitors' },
+              { label: 'Буклеты', to: '/shop/booklets' },
+              { label: 'Буклеты', to: '/shop/booklets' },
+            ],
+          },
+          {
+            label: 'Упаковка',
+            children: [
+              { label: 'Полиграфия', to: '/shop' },
+              { label: 'Баннеры', to: '/shop/banners' },
+              { label: 'Буклеты', to: '/shop/booklets' },
+              { label: 'Визитки', to: '/shop/visitors' },
+              { label: 'Буклеты', to: '/shop/booklets' },
+              { label: 'Буклеты', to: '/shop/booklets' },
+            ],
+          },
+          { label: 'Пакеты' },
+          { label: '   Бумажный пакет Великан v2' },
+        ]"
+        title="   Бумажный пакет Великан v2"
+      />
+    </div>
+    <div>
+      <div></div>
+      <div>
+        <NuxtImg />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import PageHead from "./PageHead.vue";
+</script>
+
+<style scoped></style>
---
+++ app/pages/shop/[category]/[...slug].vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="">
+    <div>
+      <ProductSlider />
+    </div>
+    <WhatsSliders :items="cases">
+      <template #card="{ item }">
+        <WhatsCard :item="item" />
+      </template>
+    </WhatsSliders>
+    <GlobalForm
+      title="Поможем подобрать оптимальные решения для вашего бизнеса"
+      description="Наш менеджер свяжется с вами и подскажет, какие продукты помогут решить ваши задачи"
+      buttonText="Отправить запрос"
+      :isColor="true"
+    />
+    <SeoBlock />
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalForm from "~/components/shared/GlobalForm.vue";
+import SeoBlock from "~/components/shared/SeoBlock.vue";
+import PageHead from "~/components/shared/PageHead.vue";
+import WhatsSliders from "~/components/blocks/WhatsSliders.vue";
+import { cases } from "~/data/Cases";
+import WhatsCard from "~/components/cards/WhatsCard.vue";
+import Btn from "~/components/buttons/Btn.vue";
+import ProductSlider from "~/components/shared/ProductSlider.vue";
+</script>
+
+<style scoped></style>
